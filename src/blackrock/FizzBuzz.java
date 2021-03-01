00b351acc59dd1a46eb8e73ba9a0d31493a07c0b
lapwa <lapwa@THINKPAD-X1-CARBON.lan>
2021-03-01 17:19:13 -0500

    2021-02-27: Amazon FizzBuzz

---
+++ src/blackrock/FizzBuzz.java
@@ -0,0 +1,23 @@
+package blackrock;
+
+public class FizzBuzz {
+
+    public static void fizzBuzz(int n) {
+    	for (int i=1;i<=n;i++) {
+    		if ( i % 3 == 0 && i % 5 == 0 )
+    			System.out.println("FizzBuzz");
+    		else if ( i % 3 == 0 && i % 5 != 0 )
+    			System.out.println("Fizz");
+    		else if ( i % 3 != 0 && i % 5 == 0 )
+    			System.out.println("Buzz");
+    		else if ( i % 3 != 0 && i % 5 != 0 )
+    			System.out.println(i);
+    	}
+    }
+	
+	public static void main(String[] args) {
+		new FizzBuzz().fizzBuzz(15);
+
+	}
+
+}
