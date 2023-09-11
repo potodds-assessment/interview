9cdfdd23ebf283afa95695c11fcfc2989dd7361d
Lap Wah Lee <lapwah.lee@gmail.com>
2023-09-11 12:40:41 -0400

    2 solutions. chatgpt and personal version.

--- src/ib/marginal_tax.java
+++ src/ib/marginal_tax.java
@@ -1,27 +1,57 @@
 package ib;
 
 public class marginal_tax {
-    public double tax_calc(double[] input1, double[] input2, double input3) {
-        double tax_amount = 0.0;
-        double current_income = input3;
+    public double marginal_tax(double[] cutoff, double[] rate, double tax) {
+        
+        double[] marginal_cutoff = new double[cutoff.length];
+        double running_tax = tax;
 
-        for(int i=1; i<input1.length; i++) {
-            double marginal = input1[i] - input1[i-1];
-            double rate = input2[i-1];
-            current_income -= marginal;
-
-            System.out.println("current_income[" + current_income + "], marignal[" + marginal + "], rate[" + rate + "]");
-
-            tax_amount +=  (current_income < marginal ? current_income : marginal) * rate;
+        for(int i=0;i<cutoff.length;i++) {
+            marginal_cutoff[i] = (i+1 >= cutoff.length) ? Double.MAX_VALUE : cutoff[i+1] - cutoff[i];
+            if (running_tax < marginal_cutoff[i]) marginal_cutoff[i] = running_tax;
+            running_tax -= marginal_cutoff[i];
+            if (running_tax < 0) break;
         }
+
+        double tax_amount = 0.0;
+
+        for(int i=0;i<marginal_cutoff.length;i++) {
+            tax_amount += rate[i] * marginal_cutoff[i];
+        }
+
         return tax_amount;
     }
 
+    public double marginal_tax_chatgpt(double[] cutoff, double[] rate, double tax) {
+        if (cutoff == null || rate == null || cutoff.length != rate.length || cutoff.length == 0) {
+            throw new IllegalArgumentException("Invalid input parameters");
+        }
+
+        double taxAmount = 0;
+        int n = cutoff.length;
+
+        for (int i = 0; i < n; i++) {
+            if (i == n - 1 || tax <= cutoff[i + 1]) {
+                taxAmount += (tax - cutoff[i]) * rate[i];
+                break;
+            } else {
+                taxAmount += (cutoff[i + 1] - cutoff[i]) * rate[i];
+            }
+        }
+
+        return taxAmount;
+    }
 
     public static void main(String[] args) {
+
+        double[] cutoff = { 0.0, 1000, 2000 };
+        double[] rate = { 0.10, 0.5, 0.20 };
+        double tax = 1500;
+
         marginal_tax app = new marginal_tax();
-        System.out.println(app.tax_calc(new double[] {0.0, 1000.0,2000.0}, new double[]{0.1, 0.5,0.2}, 4000.0));
-        System.out.println(app.tax_calc(new double[] {0.0, 1000.0}, new double[]{0.1, 0.5}, 2000.0));
+        System.out.println(app.marginal_tax(new double[]{0, 300.0}, new double[]{0.1, 0.2}, 2000.0) );
+        System.out.println(app.marginal_tax(new double[]{0, 1000.0, 2000.0, 4000.0}, new double[]{0.1, 0.5, 0.2, 0.7}, 25000.0) );
+        System.out.println(app.marginal_tax(new double[]{0, 1000.0, 2000.0}, new double[]{0.1, 0.5, 0.2}, 1500.0) );
+        System.out.println(app.marginal_tax(cutoff, rate, tax) );
     }
-    
 }
