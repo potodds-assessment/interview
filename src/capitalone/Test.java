d685bc50d39b4252e1c2719e6a63e458ac28283d
Lap Wah Lee <lapwah.lee@gmail.com>
2023-11-08 11:02:52 -0500

    Capital One Interview 11/8/2023

---
+++ src/capitalone/Test.java
@@ -0,0 +1,13 @@
+package capitalone;
+
+public class Test {
+
+    public void run(String t) {
+        System.out.println(t);
+    }
+
+    public static void main(String[] args) {
+        Test app = new Test();
+        app.run("Testing");
+    }
+}
