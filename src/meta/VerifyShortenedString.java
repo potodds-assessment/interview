b55580395d4000620e3a5be0a59aea7ad12329ff
lapwahlee <lwlee2014@gmail.com>
2024-04-26 15:00:00 -0400

    added test case

--- src/meta/VerifyShortenedString.java
+++ src/meta/VerifyShortenedString.java
@@ -94,3 +94,9 @@ public class VerifyShortenedString {
  // INVALID:
  // f6k - faceboook
  // f6k - focus
+
+// 0	1	2	3	4	5	6	7	8	9	10	11	12	13	14	15	16	17	18	19
+//i	n	t	e	r	n	a	t	i	o	n	a	l	i	z	a	t	i	o	n
+																			
+//0	1	2	3																
+//i	1	8	n																
